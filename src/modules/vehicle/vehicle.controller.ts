8f3d67f2161283dff512e5ce2159dafe306ac680
SohilaM <sohila.haddad@gmail.com>
2025-11-20 20:20:10 +0200

    feat(vehicle): implement assign and unassign driver functionalities

--- src/modules/vehicle/vehicle.controller.ts
+++ src/modules/vehicle/vehicle.controller.ts
@@ -18,6 +18,7 @@ import { CreateVehicleDto } from './dto/create-vehicle.dto';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { IdDto } from 'src/common/dtos/id.dto';
 import { updateVehicleDto } from './dto/update-vehicle.dto';
+import { AssignDriverDto } from './dto/assign-driver.dto';
 
 @Controller('vehicles')
 export class VehicleController {
@@ -64,5 +65,25 @@ export class VehicleController {
   deleteOne(@Param() params: IdDto) {
     const { id } = params;
     return this.vehicleService.deleteOne(id);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard)
+  @Patch(':id/assign-driver')
+  assignDriver(
+    @Param() params: IdDto,
+    @Body() assignDriverDto: AssignDriverDto,
+  ) {
+    const { id } = params;
+    const { driverId } = assignDriverDto;
+    return this.vehicleService.assignDriver(id, driverId);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard)
+  @Patch(':id/unassign-driver')
+  unAssignDriver(@Param() params: IdDto) {
+    const { id } = params;
+    return this.vehicleService.unAssignDriver(id);
   }
 }
--- src/modules/vehicle/vehicle.service.ts
+++ src/modules/vehicle/vehicle.service.ts
@@ -12,7 +12,6 @@ import { Model } from 'mongoose';
 import { APIResponse } from 'src/common/types/api.type';
 import { User, UserDocument } from 'src/database/schemas/user.schema';
 import { UserRole } from 'src/common/enums/user.enum';
-import { IdDto } from 'src/common/dtos/id.dto';
 import { updateVehicleDto } from './dto/update-vehicle.dto';
 
 @Injectable()
@@ -45,6 +44,12 @@ export class VehicleService {
       };
       return res;
     } catch (error) {
+      if (
+        error instanceof NotFoundException ||
+        error instanceof BadRequestException
+      )
+        throw error;
+
       if (error.code === 11000) {
         const field = Object.keys(error.keyValue || {})[0];
         const value = error.keyValue?.[field];
@@ -67,7 +72,10 @@ export class VehicleService {
   }
 
   async findOne(id: string) {
-    const vehicle = await this.vehicleModel.findById(id);
+    const vehicle = await this.vehicleModel
+      .findById(id)
+      .populate('driverId', '-password -refreshToken -__v');
+
     if (!vehicle) throw new NotFoundException('No vehicle found with this id');
 
     const res: APIResponse = {
@@ -77,11 +85,10 @@ export class VehicleService {
   }
 
   async updateOne(id: string, updateVehicleDto: updateVehicleDto) {
-    const updatedVehicle = await this.vehicleModel.findByIdAndUpdate(
-      id,
-      updateVehicleDto,
-      { new: true },
-    );
+    const updatedVehicle = await this.vehicleModel
+      .findByIdAndUpdate(id, updateVehicleDto, { new: true })
+      .populate('driverId', '-password -refreshToken -__v');
+
     if (!updatedVehicle)
       throw new NotFoundException('No vehicle found with this is');
 
@@ -99,6 +106,64 @@ export class VehicleService {
     const res: APIResponse = {
       message: 'Vehicle deleted successfully',
     };
+    return res;
+  }
+
+  async assignDriver(id: string, driverId: string) {
+    // check if user exists, and is a driver
+    const driver = await this.userModel.findById(driverId);
+
+    if (!driver) throw new NotFoundException('No driver found with this id.');
+    if (driver.role !== UserRole.DRIVER)
+      throw new BadRequestException('User with this id is not a driver.');
+
+    const isAssigned = await this.vehicleModel.findOne({
+      driverId,
+    });
+    if (isAssigned) {
+      // assigned to same vehicle
+      if (isAssigned._id.toString() === id)
+        throw new BadRequestException(
+          'Driver is already assigned to this vehicle',
+        );
+
+      // assigned to different vehicle
+      throw new BadRequestException(
+        'Driver is already assigned to another vehicle',
+      );
+    }
+
+    const updatedVehicle = await this.vehicleModel
+      .findByIdAndUpdate(id, { driverId }, { new: true })
+      .populate('driverId', '-password -refreshToken -__v');
+    if (!updatedVehicle)
+      throw new NotFoundException('No vehicle found with this id.');
+
+    const res: APIResponse = {
+      message: 'Driver assigned to vehicle successfully',
+      data: updatedVehicle,
+    };
+    return res;
+  }
+
+  async unAssignDriver(id: string) {
+    const vehicle = await this.vehicleModel.findById(id);
+    if (!vehicle) throw new NotFoundException('No vehicle found with this id.');
+    if (!vehicle.driverId)
+      throw new BadRequestException('Vehicle does not have an assigned driver');
+
+    // driverId = null
+    const updatedVehicle = await this.vehicleModel.findByIdAndUpdate(
+      id,
+      { $unset: { driverId: 1 } },
+      { new: true },
+    );
+
+    const res: APIResponse = {
+      message: 'Driver unassigned from vehicle successfully',
+      data: updatedVehicle!,
+    };
+
     return res;
   }
 }
